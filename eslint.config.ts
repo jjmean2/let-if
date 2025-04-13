edecd470de7b70768c1a88ae3622d6dea89d572e
Jongwan Lee <jjmean2@gmail.com>
2025-04-14 06:24:26 +0900

    chore: use yarn as package manager

--- eslint.config.ts
+++ eslint.config.ts
@@ -1,10 +1,12 @@
 import js from '@eslint/js'
 import type { ESLint, Linter } from 'eslint'
 import eslintConfigPrettier from 'eslint-config-prettier'
-import { defineConfig } from 'eslint/config'
+import { defineConfig, globalIgnores } from 'eslint/config'
 import tseslint from 'typescript-eslint'
 
 export default defineConfig([
+  globalIgnores(['.yarn', '.pnp.cjs', '.pnp.loader.mjs'], 'project/yarn-generated'),
+  globalIgnores(['dist'], 'project/build-artifacts'),
   {
     name: 'project/main',
     files: ['**/*.ts'],
