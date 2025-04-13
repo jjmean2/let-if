ef7ea4ccfb3476ac1d6049c58b0dc06db116cb88
Jongwan Lee <jjmean2@gmail.com>
2025-04-14 04:30:14 +0900

    Initial commit

---
+++ src/index.ts
@@ -0,0 +1,14 @@
+type FieldsNonNullable<T> = {
+  [P in keyof T]: NonNullable<T[P]>;
+};
+
+export function letIf<N extends unknown[], R>(
+  ...args: [...N, (...args: FieldsNonNullable<N>) => R]
+): R | undefined {
+  const callback = args.pop() as (...args: FieldsNonNullable<N>) => R;
+  if (args.every(($0) => $0 !== undefined && $0 !== null)) {
+    const requiredArgs = args as FieldsNonNullable<N>;
+    return callback.call(null, ...requiredArgs);
+  }
+  return undefined;
+}
